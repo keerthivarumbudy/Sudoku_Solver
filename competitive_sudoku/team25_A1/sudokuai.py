9e12a6e0a1beb930c13b1e711b4faf70523919b9
Keerthi Varumbudy <keerthi.vrmd@gmail.com>
2021-11-26 15:32:33 +0100

    fixes for eval func

--- competitive_sudoku/team25_A1/sudokuai.py
+++ competitive_sudoku/team25_A1/sudokuai.py
@@ -16,6 +16,7 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     def __init__(self):
         super().__init__()
 
+    # class minimax:
 
     def find_legal_moves(self, game_state: GameState) -> [Move]:
         N = game_state.board.N
@@ -23,13 +24,14 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         for i in range(N):
             for j in range(N):
                 if game_state.board.get(i, j) == SudokuBoard.empty:
-                    for value in range(1, N+1):
-                        if not TabooMove(i, j, value) in game_state.taboo_moves and self.is_legal(game_state, i, j, value):
+                    for value in range(1, N + 1):
+                        if not TabooMove(i, j, value) in game_state.taboo_moves and self.is_legal(game_state, i, j,
+                                                                                                  value):
                             legal_moves.append(Move(i, j, value))
 
         return legal_moves
 
-    def is_legal(self, game_state: GameState, i: int, j: int, value:int):
+    def is_legal(self, game_state: GameState, i: int, j: int, value: int):
         N = game_state.board.N
         m = game_state.board.m
         n = game_state.board.n
@@ -41,53 +43,56 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
 
         block_row = i // m
         block_column = j // n
-        for k in range(block_row*m, block_row*m + m):
-            for o in range(block_column*n, block_column*n + n):
+        for k in range(block_row * m, block_row * m + m):
+            for o in range(block_column * n, block_column * n + n):
                 if game_state.board.get(k, o) == value:
                     return False
         return True
 
-
-    def evaluation_function(self, board, move):
+    def evaluate_move(self, board, move):
         score_dict = {
-            0 : 0,
-            1 : 1,
-            2 : 3,
-            3 : 7
+            0: 0,
+            1: 1,
+            2: 3,
+            3: 7
         }
         m = board.m
         n = board.n
         N = board.N
         count = 0
         for i in range(0, N):
-           if board.get(i,move.j)== SudokuBoard.empty:
+            if board.get(i, move.j) == SudokuBoard.empty and i!=move.i:
                 break
         else:
+            print("first else")
             count += 1
 
         for j in range(0, N):
-           if board.get(move.i,j)== SudokuBoard.empty:
+            if board.get(move.i, j) == SudokuBoard.empty and j!=move.j:
                 break
         else:
+            print("second else")
             count += 1
 
-        block_range = [i // m, j// n ]
+        block_range = [move.i // m, move.j // n]
 
-        for k in range(block_range[0]*m, block_range[0]*m + m):
-            for o in range(block_range[1]*n, block_range[1]*n + n):
-                if board.get(k, o) == SudokuBoard.empty:
-                   break
+        for k in range(block_range[0] * m, block_range[0] * m + m):
+            counter = 0
+            for o in range(block_range[1] * n, block_range[1] * n + n):
+                print("within block : ", k,o)
+                if board.get(k, o) == SudokuBoard.empty and (k,o) != (move.i,move.j) :
+                    print("breaking!")
+                    counter = 1
+                    break
+            if counter == 1:
+                print("breaking!")
+                break
         else:
+            print("third else")
             count += 1
-
 
         score = score_dict[count]
         return score
-
-
-    def minimax(self):
-        return
-
 
     # N.B. This is a very naive implementation.
     def compute_best_move(self, game_state: GameState) -> None:
@@ -97,18 +102,13 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
             print(move.i, move.j, move.value)
 
         def possible(i, j, value):
-            return game_state.board.get(i, j) == SudokuBoard.empty and not TabooMove(i, j, value) in game_state.taboo_moves
+            return game_state.board.get(i, j) == SudokuBoard.empty and not TabooMove(i, j,
+                                                                                     value) in game_state.taboo_moves
 
-        all_moves = [Move(i, j, value) for i in range(N) for j in range(N) for value in range(1, N+1) if possible(i, j, value)]
+        all_moves = [move for move in legal_moves if possible(move.i, move.j, move.value)]
         move = random.choice(all_moves)
         self.propose_move(move)
-        print("evaluate:")
-        print(self.evaluation_function(game_state.board, move))
+        print(self.evaluate_move(game_state.board, move), "= score")
         while True:
             time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
-
-
-
-
-
+            self.propose_move(move)
