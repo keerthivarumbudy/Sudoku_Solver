156c137afde5818ba01018d9043f277386ddaee5
Weijts <k.weijts@student.tue.nl>
2021-11-30 15:16:46 +0100

    Taboo_moves fixed

--- competitive_sudoku/team25_A1/sudokuai.py
+++ competitive_sudoku/team25_A1/sudokuai.py
@@ -117,7 +117,7 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
                     return False
         return True
 
-    def minimax_alpha_beta(self, game_state: GameState, max_depth, current_depth, alpha, beta):
+    def minimax_alpha_beta(self, game_state: GameState, max_depth, current_depth, alpha, beta, initial_gamestate):
         best_move = None
         if current_depth == max_depth or self.is_board_full(game_state):
             return self.evaluation_function(game_state)
@@ -126,51 +126,56 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
 
         if (self.player_number - len(game_state.moves)) % 2 == 1:  # our turn
             if len(moves) == 0:  # if AI_agent cannot find one legal move
-                return math.inf  # because there is a taboo in recursion
+                return self.evaluation_function(initial_gamestate) # because there is a taboo in recursion
 
             current_max = -math.inf   # default current_max = negative inf
             for move in moves:  # find a move maximize the min
-                # print("I am thinking the move:"+str(move)+"in depth:"+str(current_depth))
+                print("I am thinking the move:"+str(move)+"in depth:"+str(current_depth))
                 #   copy and update game_state for next depth of each move:
                 new_game_state = copy.deepcopy(game_state)
                 new_game_state.board.put(move.i, move.j, move.value)
                 new_game_state.scores[self.player_number - 1] += self.compute_move_score(game_state.board, move)
                 new_game_state.moves.append(move)
-                eval_value = self.minimax_alpha_beta(new_game_state, max_depth, current_depth + 1, alpha, beta)
-                # print("eval is:"+str(b))
+                eval_value = self.minimax_alpha_beta(new_game_state, max_depth, current_depth + 1, alpha, beta, initial_gamestate)
+                print("eval is:"+str(eval_value))
                 #   compare the best current_max and the eval_value of current move:
                 #print("current_depth ", current_depth)
                 if current_depth == 0 and eval_value > current_max:    # propose a current best move in depth=0
+                    print("minimax get a move which get:" + str(eval_value) + " points")
                     best_move = move
+                    print("current best move: ", best_move.i, best_move.j, best_move.value)
                     if max_depth == 1:
                         self.propose_move(move)
 
                 current_max = max(current_max, eval_value)
+
                 if current_max >= beta:
+                    print("test")
                     return current_max
                 if current_max > alpha:
                     alpha = current_max
             if current_depth == 0:
+                print("something")
                 self.propose_move(best_move)
             return current_max
 
         else:   # opponent's turn
             if len(moves) == 0:  # if AI_agent cannot find one legal move
-                return -math.inf  # because there is a taboo in recursion
+                return self.evaluation_function(initial_gamestate) # because there is a taboo in recursion
 
-            # print("now I think as opponent:")
+            print("now I think as opponent:")
             current_min = math.inf    # default current_min = positive inf
             for move in moves:  # find a move minimize the max
-                # print("if opponent take move:"+str(move)+" in depth:"+str(current_depth))
+                print("if opponent take move:"+str(move)+" in depth:"+str(current_depth))
                 #   copy and update game_state for next depth of each move:
                 new_game_state = copy.deepcopy(game_state)
                 new_game_state.board.put(move.i, move.j, move.value)
                 new_game_state.scores[self.opponent_number - 1] += self.compute_move_score(game_state.board, move)
                 new_game_state.moves.append(move)
                 #   compare the current_min and the eval_value of current move:
-                current_min = min(current_min, self.minimax_alpha_beta(new_game_state, max_depth, current_depth + 1,alpha, beta))
-                # print("my advantage so far is:"+str(a)+" points")
-            # print("I think after opponent moved, my advantage is:"+str(a)+" points")
+                current_min = min(current_min, self.minimax_alpha_beta(new_game_state, max_depth, current_depth + 1,alpha, beta, initial_gamestate))
+                print("my advantage so far is:"+str(current_min)+" points")
+                print("I think after opponent moved, my advantage is:"+str(current_min)+" points")
                 if current_min <= alpha:
                     return current_min
                 if current_min < beta:
@@ -188,8 +193,10 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
 
         # To know the order of our AI_agent player and opponent player:
         [self.player_number, self.opponent_number] = (1, 2) if len(game_state.moves) % 2 == 0 else (2, 1)
-        # print("our player_number is:" + str(self.player_number))
-        depth = 0   # set the max_depth for minimax()
-        while True:
-            # run the minimax()
-            self.minimax_alpha_beta(game_state, depth + 1, 0, -math.inf, math.inf)
+        print("our player_number is:" + str(self.player_number))
+        depth = 2   # set the max_depth for minimax()
+        initial_gamestate = copy.deepcopy(game_state)
+        self.minimax_alpha_beta(game_state, depth + 1, 0, -math.inf, math.inf, initial_gamestate)
+        # while True:
+        #     # run the minimax()
+        #     self.minimax_alpha_beta(game_state, depth + 1, 0, -math.inf, math.inf)
