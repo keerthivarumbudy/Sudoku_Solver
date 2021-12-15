32bb635c91093595bbc59a6d9d7db3945286e004
Suzheyuan <ed.suzy0@gamil.com>
2021-12-15 15:45:50 +0100

    final code

--- competitive_sudoku/team25_A2/sudokuai.py
+++ competitive_sudoku/team25_A2/sudokuai.py
@@ -273,6 +273,5 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         while True:
             # run the minimax()
             # print("depth: ", depth)
-            self.minimax_alpha_beta(game_state, depth, 0, -math.inf, math.inf, real_diff_score, initial_legal_moves)
-            # print("move_left:"+str(self.moves_left))
+           # print("move_left:"+str(self.moves_left))
             depth += 1
